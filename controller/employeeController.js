1465fd3d44449733bd1a83f5bc8832795a1ae443
KryzAle <kryzale@gmail.com>
2022-02-11 22:08:12 -0500

    Finish but...without final array

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,11 +1,14 @@
 function seekCoincidence(employees) {
   const pairsEmployee = convertPairs(employees);
-   /*for (let i = 0; i <= pairsEmployee.length - 1; i++) {
+  //console.log(pairsEmployee);
+  for (let i = 0; i <= pairsEmployee.length - 1; i++) {
     //console.log(i);
-    getTimes(pairsEmployee[i][0]._schedule, pairsEmployee[i][1]._schedule);
-  }*/
+    let timesCoincidences = getTimes(pairsEmployee[i][0]._schedule, pairsEmployee[i][1]._schedule);
+    console.log(pairsEmployee[i][0]._name+"-"+pairsEmployee[i][1]._name+":"+timesCoincidences);
+  }
 
-  getTimes(pairsEmployee[0][0]._schedule, pairsEmployee[0][1]._schedule);
+  //let timesCoincidences = getTimes(pairsEmployee[0][0]._schedule, pairsEmployee[0][1]._schedule);
+  //console.log(timesCoincidences);
 }
 
 function convertPairs(employeesObj) {
@@ -19,10 +22,53 @@ function convertPairs(employeesObj) {
   }
   return employePairs;
 }
-function getTimes(schedule1, schedule2) {
-  console.log(schedule1);
-  console.log(schedule2);
+function getTimes(firstSchedule, secondSchedule) {
+  let coincidences = 0;
+  //console.log(firstSchedule);
+  //console.log(secondSchedule);
+  let newFirstSched = changeFormat(firstSchedule);
+  let newSecSched = changeFormat(secondSchedule);
 
+  if (newFirstSched.length != newSecSched.length) {
+    if (newFirstSched.length < newSecSched.length) {
+      newSecSched = newFirstSched
+        .map((el) => newSecSched.filter((element) => element[0] == el[0]))
+        .map((element) => element[0]);
+    } else {
+      newFirstSched = newSecSched
+        .map((el) => newFirstSched.filter((element) => element[0] == el[0]))
+        .map((element) => element[0]);
+    }
+  }
+  //console.log(newFirstSched);
+  //console.log(newSecSched);
+
+  for (i = 0; i <= newFirstSched.length - 1; i++) {
+    let hourInFirstSchedule = new Date("2020-01-01 " + newFirstSched[i][1]);
+    let hourOutFirstSchedule = new Date("2020-01-01 " + newFirstSched[i][2]);
+    let hourInSecSchedule = new Date("2020-01-01 " + newSecSched[i][1]);
+    let hourOutSecSchedule = new Date("2020-01-01 " + newSecSched[i][2]);
+
+    if (
+      hourInSecSchedule.getTime() >= hourInFirstSchedule.getTime() &&
+      hourInSecSchedule.getTime() < hourOutFirstSchedule.getTime()
+    ) {
+      coincidences++;
+    } else if (
+      hourInFirstSchedule.getTime() >= hourInSecSchedule.getTime() &&
+      hourInFirstSchedule.getTime() < hourOutSecSchedule
+    ) {
+      coincidences++;
+    } else {
+    }
+  }
+  return coincidences;
 }
-
+function changeFormat(schedule) {
+  return schedule.map((element) =>
+    element
+      .split(new RegExp("(MO|TU|WE|TH|FR|SA|SU|-)"))
+      .filter((item) => item !== "" && item !== "-")
+  );
+}
 module.exports = seekCoincidence;
